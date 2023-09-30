0a0cbbec82fd159c7bb406ca547c89eb48afe629
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-30 10:29:09 +0330

    add: xss attack

--- data/attacks/xss.sh
+++ data/attacks/xss.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-echo "This is XSS";
+
+# message
+echo "Starting XSS attack ...";
+
+echo "performing at:";
+
+# print date to set attack
+date "+%Y/%m/%d %H:%M:%S";
+
+# make a single requests to perform XSS attack
+HOST1="$1"
+RESP1=$(curl -s "$HOST1")
+
+echo "Got $RESP1";
+
+if [ "$RESP1" == "*<html>*" ]
+then
+  echo "Could add the following script which is a malware to your response!"
+  echo "<script>for() { alert('got hacked!') }</script>"
+  exit 0;
+fi
+
+exit 1;
