9a5800f11fe4f1a0eebb699f50d0530e126b284f
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-30 10:30:25 +0330

    add: zero day attack

--- data/attacks/zero-day.sh
+++ data/attacks/zero-day.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-echo "This is zero day!";
+
+# get host to perform attack
+HOST=$1
+
+# message
+echo "Starting Zero Day attack ...";
+
+echo "performing at:";
+
+# print date to set attack
+date "+%Y/%m/%d %H:%M:%S";
+
+# make a loop to perform zero-day attack
+for _ in {1..100}
+do
+  TMP=$(curl -X GET "$HOST")
+  echo Got: "$TMP"
+done
+
+echo "System did not block me!";
+
+exit 0;
