3f2ab697feaf80c691de8dea525eb099abc8960d
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-30 09:59:17 +0330

    add: LP attack

--- data/attacks/dos.sh
+++ data/attacks/dos.sh
@@ -12,7 +12,7 @@ echo "performing at:";
 # print date to set attack
 date "+%Y/%m/%d %H:%M:%S";
 
-# make a loop to perform DDoS attack
+# make a loop to perform DoS attack
 for _ in {1..100}
 do
   TMP=$(curl -X GET "$HOST")
--- data/attacks/large-payload.sh
+++ data/attacks/large-payload.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-echo "This is Large payload!";
+
+# get host to perform attack
+HOST=$1
+
+# message
+echo "Starting BGP attack ...";
+
+echo "performing at:";
+
+# print date to set attack
+date "+%Y/%m/%d %H:%M:%S";
+
+# make a loop to perform LP attack
+for i in {1..10}
+do
+  BYTES=$((i * 100))
+  echo "Sending $BYTES Gb data into $HOST"
+
+  TMP=$(curl -X POST "$HOST")
+  echo Got: "$TMP"
+done
+
+exit 0;
