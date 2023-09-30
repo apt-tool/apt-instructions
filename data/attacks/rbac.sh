fb46b8f200662dcb4b5cf429742ceb6054fb28aa
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-30 10:18:36 +0330

    add: RBAC attack

--- data/attacks/rbac.sh
+++ data/attacks/rbac.sh
@@ -1,3 +1,96 @@
 #!/usr/bin/env bash
 
-echo "This is RBAC";
+
+# message
+echo "Starting RBAC attack ...";
+
+echo "performing at:";
+
+# print date to set attack
+date "+%Y/%m/%d %H:%M:%S";
+
+# make a single requests to perform RBAC attack
+HOST1="$1/login"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/signin"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/auth"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/auth/login"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/auth/signin"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/signup"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/auth/signup"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/register"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/auth/register"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+echo "Secure RBAC."
+
+exit 1;
