2c5232bb3b78a6d7fae8cecfcb7131f7550102ad
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-10 11:45:48 +0330

    add: rbac attack

---
+++ data/attacks/rbac.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+
+# message
+echo "Starting RBAC attack ...";
+
+echo "performing at:";
+
+# print date to set attack
+date "+%Y/%m/%d %H:%M:%S";
+
+# make a single requests to perform RBAC attack
+HOST1="$1/login"
+RESP1=$(curl "$HOST1")
+
+echo "$HOST1";
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+sleep 5s;
+
+HOST1="$1/signin"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+sleep 5s;
+
+HOST1="$1/auth"
+RESP1=$(curl "$HOST1")
+
+echo "$HOST1";
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+sleep 5s;
+
+HOST1="$1/auth/login"
+RESP1=$(curl "$HOST1")
+
+echo "$HOST1";
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+sleep 5s;
+
+HOST1="$1/auth/signin"
+RESP1=$(curl "$HOST1")
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+sleep 5s;
+
+HOST1="$1/signup"
+RESP1=$(curl "$HOST1")
+
+echo "$HOST1";
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+sleep 5s;
+
+HOST1="$1/auth/signup"
+RESP1=$(curl "$HOST1")
+
+echo "$HOST1";
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/register"
+RESP1=$(curl "$HOST1")
+
+echo "$HOST1";
+
+sleep 5s;
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+HOST1="$1/auth/register"
+RESP1=$(curl "$HOST1")
+
+echo "$HOST1";
+
+sleep 5s;
+
+if [ "$RESP1" == "HTTP/1.1 200" ] || [ "$RESP1" == "HTTP/1.1 202" ]
+then
+  echo "Got $RESP1";
+  exit 0;
+fi
+
+sleep 5s;
+
+echo "Secure on RBAC attack."
+
+exit 1;
