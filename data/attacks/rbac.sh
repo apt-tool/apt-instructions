7eba6b2f7011fb062f498480d9c2061fa9b66626
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-10 17:30:49 +0330

    add: system status in attacks

--- data/attacks/rbac.sh
+++ data/attacks/rbac.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
 
 
+echo "System Status Report:"
+echo "---------------------"
+
+# Display current date and time
+echo "Date and Time:"
+date
+
+# Display system uptime
+echo -e "\nSystem Uptime:"
+uptime
+
+# Display system load average
+echo -e "\nSystem Load Average:"
+cat /proc/loadavg
+
+# Display memory usage
+echo -e "\nMemory Usage:"
+free -h
+
+# Display disk space usage
+echo -e "\nDisk Space Usage:"
+df -h
+
+# Display top CPU-consuming processes
+echo -e "\nTop CPU-consuming Processes:"
+ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 6
+
+# Display top memory-consuming processes
+echo -e "\nTop Memory-consuming Processes:"
+ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%mem | head -n 6
+
+
+
 # message
 echo "Starting RBAC attack ...";
 
