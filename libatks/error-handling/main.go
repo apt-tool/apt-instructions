8b4be3c478363a8445a615439584cb896d4f359d
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-26 21:03:38 +0330

    add: protocol change in error handling

--- libatks/error-handling/main.go
+++ libatks/error-handling/main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"golang.org/x/net/http2"
 )
 
 type CallBack func(url string) bool
@@ -105,22 +107,97 @@ func dirListing(url string) bool {
 	return false
 }
 
+func protocol(url string) bool {
+	// Create an HTTP/2-enabled client
+	client := &http.Client{
+		Transport: &http2.Transport{},
+	}
+
+	// Create an HTTP request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println("Error creating request:", err)
+
+		return false
+	}
+
+	// Set HTTP/2 protocol for the request
+	req.Header.Set("Connection", "Upgrade, HTTP2-Settings")
+	req.Header.Set("Upgrade", "h2c")
+	req.Header.Set("HTTP2-Settings", "")
+
+	// Send the HTTP request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error sending request:", err)
+
+		return true
+	}
+
+	defer resp.Body.Close()
+
+	// Print the response status code
+	log.Println("Response Status:", resp.Status)
+
+	return true
+}
+
 func fileFetch(url string) bool {
 	paths := []string{
-		"a/b",
-		"../a/b",
-		"../a/../b",
-		"a//c",
-		"/",
-		"a/c/.",
-		"/var/",
-		"/etc/",
-		"/dev/",
-		"/usr/",
-		"/lib/",
-		"/proc/",
-		"/Users/",
-		"/System",
+		"/bin",
+		"/boot",
+		"/etc/bashrc",
+		"/etc/fstab",
+		"/etc/group",
+		"/etc/hostname",
+		"/etc/hosts",
+		"/etc/passwd",
+		"/etc/profile",
+		"/etc/resolv.conf",
+		"/etc/shadow",
+		"/etc/sudoers",
+		"/proc",
+		"/sys",
+		"/usr/bin",
+		"/usr/share/doc",
+		"/var/lib/dpkg/status",
+		"/var/lib/rpm",
+		"/var/log",
+		"/var/spool/mail",
+		"/etc/apache2/apache2.conf",
+		"/etc/apache2/sites-available/",
+		"/var/log/apache2/access.log",
+		"/var/log/apache2/error.log",
+		"/etc/apache2/mods-available/",
+		"/etc/apache2/ssl/",
+		"/var/www/html/",
+		"/etc/apache2/conf-available/",
+		"/etc/apache2/mime.types",
+		"/etc/apache2/httpd.conf.dpkg-dist",
+		"/etc/nginx/nginx.conf",
+		"/etc/nginx/sites-available/",
+		"/var/log/nginx/access.log",
+		"/var/log/nginx/error.log",
+		"/etc/nginx/conf.d/",
+		"/etc/nginx/mime.types",
+		"/etc/nginx/fastcgi_params",
+		"/etc/nginx/scgi_params",
+		"/etc/nginx/uwsgi_params",
+		"C:\\Windows\\System32\\config\\SYSTEM",
+		"C:\\Windows\\System32\\config\\SOFTWARE",
+		"C:\\Windows\\System32\\config\\SAM",
+		"C:\\Windows\\System32\\config\\SECURITY",
+		"C:\\Windows\\System32\\config\\DEFAULT",
+		"C:\\Windows\\System32\\drivers\\etc\\hosts",
+		"C:\\Windows\\System32\\drivers\\etc\\services",
+		"C:\\Windows\\System32\\drivers\\etc\\networks",
+		"C:\\Windows\\System32\\drivers\\etc\\lmhosts",
+		"C:\\Windows\\System32\\GroupPolicy\\",
+		"C:\\Windows\\System32\\GroupPolicyUsers\\",
+		"C:\\Windows\\System32\\Tasks\\",
+		"C:\\Windows\\System32\\drivers\\etc\\protocol",
+		"C:\\Windows\\System32\\taskschd.msc",
+		"C:\\Windows\\System32\\inetsrv\\config\\applicationHost.config",
 	}
 
 	for _, subPath := range paths {
@@ -146,6 +223,7 @@ func main() {
 	callbacks := []CallBack{
 		fileFetch,
 		dirListing,
+		protocol,
 		fileSend,
 	}
 
--- libatks/go.mod
+++ libatks/go.mod
@@ -5,4 +5,6 @@ go 1.20
 require (
 	github.com/machinebox/graphql v0.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
